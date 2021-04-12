9be6f8e19de5d4a1a187c526e6d9c4a69a894661
globa <rvparikh@wpi.edu>
2021-04-12 10:57:05 -0400

    test

--- app/src/main/java/com/example/appsforgood/SuggestionActivity.java
+++ app/src/main/java/com/example/appsforgood/SuggestionActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SuggestionActivity extends AppCompatActivity {
 
     @Override
+    // Open survey at proper time
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_survey);
