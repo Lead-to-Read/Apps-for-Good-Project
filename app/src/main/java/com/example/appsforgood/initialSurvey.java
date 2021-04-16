4c79dfad63e2e955927cad08365a7bb1b918452a
globa <rvparikh@wpi.edu>
2021-04-16 19:57:31 -0400

    More coming

--- app/src/main/java/com/example/appsforgood/initialSurvey.java
+++ app/src/main/java/com/example/appsforgood/initialSurvey.java
@@ -20,22 +20,32 @@ import java.util.ArrayList;
 
 public class initialSurvey extends AppCompatActivity {
 
-    //private Book b;
+    // Instance Variables
     private ArrayList<Book> correctLangBooks = new ArrayList<Book>();
-    private ArrayList<Integer> bookScores = new ArrayList<Integer>();
-    private int authorSubRating;
-    private int authorRating;
+    private ArrayList<Double> bookScores = new ArrayList<Double>();
+    private String preferredLength;
+    private int lowerPageBound;
+    private int upperPageBound;
 
     @Override
+    /**
+     * Displays activity from activity_initial_survey.xml
+     * @param savedInstanceState used to open correct xml
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_survey);
     }
 
+    /**
+     * Calls all methods used after the initial survey is completed by the user
+     * @param v
+     */
     public void onContinueClick (View v) {
         //Make ArrayList, filtering out books with the incorrect language
         langFilter();
         authorScore();
+        lengthScore();
         Intent start = new Intent(this, DisplayBooks.class);
         startActivity(start);
     }
@@ -67,10 +77,11 @@ public class initialSurvey extends AppCompatActivity {
         for (Book b : correctLangBooks) {
             String bookAuthor = b.getAuthors().replaceAll("\\s", "");
             Log.v("Author", "Book author: " + bookAuthor);
+            double authorSubRating;
             if (userAuthorNew.contains(bookAuthor)) {
-                authorSubRating = 2;
-            } else {
                 authorSubRating = 1;
+            } else {
+                authorSubRating = 0;
             }
             Log.v("Author", "Subrating: " + authorSubRating);
 
@@ -79,8 +90,56 @@ public class initialSurvey extends AppCompatActivity {
             int authorUserRankingInt = authorUserRanking.getProgress();
             Log.v("Author", "Subrating: " + authorUserRankingInt);
             double authorRating = authorSubRating * authorUserRankingInt;
+            bookScores.add(authorRating);
             Log.v("Author", "Author Score: " + authorRating);
         }
     }
-}
 
+    public void shortLengthScore(View v) {
+        preferredLength = "Short";
+        lowerPageBound = 0;
+        upperPageBound = 200;
+        Log.v("Check", "Length " + preferredLength);
+    }
+
+    public void mediumLengthScore(View v) {
+        preferredLength = "Medium";
+        lowerPageBound = 201;
+        upperPageBound = 500;
+    }
+
+    public void longLengthScore(View v) {
+        preferredLength = "Long";
+        lowerPageBound = 501;
+        upperPageBound = 100000; // Safe upper bound for largest book
+    }
+
+    public void lengthScore() {
+        Log.v("Check", "Length Books " + correctLangBooks.size());
+        for (int index = 0; index < correctLangBooks.size(); index++) {
+            int currentPageLength = correctLangBooks.get(index).getNumPages();
+            double subLengthRating = 0;
+            Log.v("CheckD", "Length" + currentPageLength);
+            if (lowerPageBound <= currentPageLength && currentPageLength <= upperPageBound) {
+                subLengthRating = 1;
+            } else if (lowerPageBound - 25 <= currentPageLength && currentPageLength <= upperPageBound + 25) {
+                subLengthRating = 0.75;
+            } else if (lowerPageBound - 50 <= currentPageLength && currentPageLength <= upperPageBound + 50) {
+                subLengthRating = 0.5;
+            } else if (lowerPageBound - 75 <= currentPageLength && currentPageLength <= upperPageBound + 75) {
+                subLengthRating = 0.25;
+            } else {
+                subLengthRating = 0;
+            }
+            Log.v("Check", "Length" + subLengthRating);
+            ProgressBar lengthUserRanking = findViewById(R.id.bookLengthRankingSlider);
+            int lengthUserRankingInt = lengthUserRanking.getProgress();
+            double lengthRating = (subLengthRating * lengthUserRankingInt);
+            Log.v("CheckA", "Length" + subLengthRating);
+            double currentTotalRating = bookScores.get(index);
+            bookScores.set(index, currentTotalRating + lengthRating);
+        }
+        Log.v("Check", "LengthR");
+	}
+
+}
