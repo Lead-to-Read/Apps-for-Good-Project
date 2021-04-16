8aa3c966443d3583ad933fbb7f9155456c288748
globa <rvparikh@wpi.edu>
2021-04-16 09:14:45 -0400

    update

--- app/src/main/java/com/example/appsforgood/Manager.java
+++ app/src/main/java/com/example/appsforgood/Manager.java
@@ -9,8 +9,8 @@ public class Manager extends Application {
     static ArrayList<Book> books = new ArrayList<Book>();
 
     /**
-     * Returns arraylist
-     * @return
+     * gets arraylist of books
+     * @return arraylist of books
      */
     public static ArrayList<Book> getBooks() {
         return books;
@@ -18,7 +18,7 @@ public class Manager extends Application {
 
     /**
      * Adds a book to the arraylist
-     * @param book
+     * @param book contains the book to be added
      */
     public void addBook(Book book) {
         books.add(book);
